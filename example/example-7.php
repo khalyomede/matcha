2bb3c67ca12461c0911bf7426b97ff12cfaf61e2
khalyomede <>
2018-09-29 23:52:14 +0200

    added example 7 on testing against a specific format

---
+++ example/example-7.php
@@ -0,0 +1,13 @@
+<?php
+    require(__DIR__ . '/../vendor/autoload.php');
+
+    use function Khalyomede\Style\expect;
+
+    describe('json', function() {
+        it('should be a valid json string', function() {
+            expect('{"hello": "world"}')->toBe()->aString()->inJsonFormat();
+        });
+    });
+
+    run();
+?>
