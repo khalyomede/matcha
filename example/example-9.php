bb0784ce2c4d9f69df1123a932ce4496f61940c6
khalyomede <>
2018-10-02 23:13:59 +0200

    added example on how to use the report level to augment the detail of the report in consoel

---
+++ example/example-9.php
@@ -0,0 +1,27 @@
+<?php
+    require(__DIR__ . '/../vendor/autoload.php');
+
+    use function Khalyomede\Style\expect;
+    use Khalyomede\ReportLevel;
+
+    describe('array', function() {
+        it('should merge two arrays', function() {
+            expect( array_merge([1, 2, 3], [4, 5, 6]) )->toBe()->equalTo([1, 2, 3, 4, 5, 6]);
+        });
+
+        it('should diff two array', function() {
+            expect( array_count_values([1, 1, 3]) )->toBe()->equalTo([1 => 2, 3 => 1]);
+        });
+
+        it('should shuffle an array', function() {
+            $array = [1, 2];
+
+            expect( shuffle($array) )->toBe()->anArray();
+        });
+    });
+
+    report('detailed');
+    // report(ReportLevel::DETAILED);
+
+    run();
+?>
